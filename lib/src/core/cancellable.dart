e54cbeb69d8a68a9ad36e18c7e86de5002a3e7e4
aymtools <anyaming0@gmail.com>
2023-08-07 21:06:12 +0800

    新增释放资源功能，cancel相关功能取消

--- lib/src/core/cancellable.dart
+++ lib/src/core/cancellable.dart
@@ -1,5 +1,31 @@
 import 'dart:async';
 
+///永远也不会执行的 Future
+class _NeverExecFuture<T> implements Future<T> {
+  @override
+  Stream<T> asStream() => StreamController<T>().stream;
+
+  @override
+  Future<T> catchError(Function onError, {bool Function(Object error)? test}) =>
+      this;
+
+  @override
+  Future<R> then<R>(FutureOr<R> Function(T value) onValue,
+      {Function? onError}) {
+    return _NeverExecFuture<R>();
+  }
+
+  @override
+  Future<T> timeout(Duration timeLimit, {FutureOr<T> Function()? onTimeout}) {
+    return this;
+  }
+
+  @override
+  Future<T> whenComplete(FutureOr<void> Function() action) {
+    return this;
+  }
+}
+
 ///用于取消
 class Cancellable {
   final Completer _completer = Completer();
@@ -11,20 +37,23 @@ class Cancellable {
   bool get isCancelled => _isCancelled;
 
   ///当取消时的处理
-  Future get whenCancel => _completer.future;
+  Future get whenCancel => _isReleased ? _NeverExecFuture() : _completer.future;
 
   void Function()? _notifyCancelled;
+
+  bool _isReleased = false;
 
   ///通知执行取消
   void cancel() => _cancel(true);
 
   void _cancel(bool notifyCancelled) {
-    if (_isCancelled == true) return;
+    if (_canNotUse) return;
     _isCancelled = true;
     _completer.complete();
     _caches
         .map((element) => element.target)
-        .where((element) => element != null && !element.isCancelled)
+        .where((element) =>
+            element != null && !element.isCancelled && !element._isReleased)
         .forEach((element) => element?._cancel(false));
     _caches.clear();
 
@@ -39,7 +68,9 @@ class Cancellable {
   ///[father] 同时接受两个上级取消的控制 有任意其他取消的时候新的也执行取消
   ///[infectious] 传染 当新的able执行取消的时候将生产者同时取消
   Cancellable makeCancellable({Cancellable? father, bool infectious = false}) {
+    _releaseCache();
     Cancellable c = Cancellable();
+    if (_isReleased) return c;
     if (isCancelled || (father != null && father.isCancelled)) {
       Future.delayed(const Duration(), () => c._cancel(false));
     } else {
@@ -64,6 +95,18 @@ class Cancellable {
     _caches.removeWhere((c) => c.target == cancellable);
   }
 
-  void _releaseCache() =>
-      _caches.removeWhere((c) => c.target?.isCancelled ?? true);
+  bool get _canNotUse => isCancelled || _isReleased;
+
+  void _releaseCache() {
+    if (_canNotUse) return;
+    _caches.removeWhere((c) => c.target?._canNotUse ?? true);
+  }
+
+  // 施放资源 当前able不在使用
+  void release({bool notifyToChild = true}) {
+    if (_canNotUse) return;
+    _isReleased = true;
+    if (notifyToChild) _caches.forEach((c) => c.target?.release());
+    _caches.clear();
+  }
 }
