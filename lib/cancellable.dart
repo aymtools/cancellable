dd3be9971cd4e595af88b21abf254fd4111b25e8
aymtools <anyaming0@gmail.com>
2023-11-18 21:36:33 +0800

    新增定义已取消时可用的抛出异常

--- lib/cancellable.dart
+++ lib/cancellable.dart
@@ -3,3 +3,4 @@ library cancellable;
 export 'src/core/cancellable.dart' hide NeverExecFuture;
 export 'src/core/future_ext.dart' ;
 export 'src/core/stream_ext.dart' ;
+export 'src/core/cancelled_exception.dart' ;
